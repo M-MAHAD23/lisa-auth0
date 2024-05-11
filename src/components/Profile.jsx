42e0d7c847ced5fe2b68b3d2a090b5c861cbac23
mmahad911 <mmahad913@gmail.com>
2024-05-11 19:00:22 +0500

    Test Data!
    Test Data!

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      console.log(user);
+    }
+  }, [isAuthenticated]); // Run only when isAuthenticated changes
 
   if (isLoading) {
     return <div>Loading ...</div>;
