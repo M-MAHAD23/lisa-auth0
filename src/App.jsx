e3fa2c31b253bb05fabb29eccc1f89ccb233af25
mmahad911 <mmahad913@gmail.com>
2024-05-11 19:28:18 +0500

    Login Automated.
    Login Automated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react"
 import LoginButton from "./components/LoginButton"
 import LogoutButton from "./components/LogoutButton"
 import Profile from "./components/Profile"
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 function App() {
+  const { loginWithRedirect } = useAuth0();
+  useEffect(() => {
+    loginWithRedirect()
+  })
   return (
     <>
-      <LoginButton />
+      {/* <LoginButton /> */}
       <LogoutButton />
       <Profile />
     </>
